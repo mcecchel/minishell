a6795351349ba2988ff8b9c47c126a418f3af781
Matteo <mbrighi@student.42firenze.it>
2025-06-10 15:10:28 +0200

    merge main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mbrighi <mbrighi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 17:30:08 by mcecchel          #+#    #+#             */
-/*   Updated: 2025/06/10 15:03:02 by mbrighi          ###   ########.fr       */
+/*   Updated: 2025/06/10 15:09:53 by mbrighi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,30 @@ void sigint_handler(int sig)
 	rl_redisplay();                  // Mostra il prompt di nuovo
 }
 
+void	init_shell(t_shell *shell, char **envp)
+{
+	shell->envp = envp;
+	shell->cmd = NULL;
+	shell->n_cmds = 0;
+	shell->in_quote = false;
+	shell->token.head = NULL;
+	shell->token.current = NULL;
+}
+
+void	cleanup_shell(t_shell *shell)
+{
+	if (shell->cmd)
+	{
+		free_cmd_list(shell->cmd);
+		shell->cmd = NULL;
+	}
+	if (shell->token.head)
+	{
+		free_tokens(shell->token.head);
+		shell->token.head = NULL;
+		shell->token.current = NULL;
+	}
+}
 
 void print_pwd(t_env *env_list)
 {
@@ -97,12 +121,92 @@ void	parser_builtin(t_shell *root, char *read_line)
 		//printf("%s\n", read_line);
 }
 
-
 int main(int argc, char **argv, char **envp)
 {
-    char *read_line;
-    read_line = readline("Prompt > ");
-    printf("%s\n", read_line);
-	free(read_line);
+    char    *line;
+    t_shell shell;
+    
+    (void)argc;
+    (void)argv;
+    init_shell(&shell, envp);
+    while (1)
+    {
+        line = readline("minishell> ");
+        if (!line)
+        {
+            printf("\nExiting...\n");
+            break;
+        }
+        // Ignora linee vuote
+        if (*line == '\0')
+        {
+            free(line);
+            continue;
+        }
+        add_history(line);
+        // Tokenizza l'input
+        if (!tokenize_input(&shell.token, line))
+        {
+            ft_printf("Error: Tokenization failed\n");
+            free(line);
+            continue;
+        }
+        // Debug opzionale
+        #ifdef DEBUG
+        printf("Generated tokens:\n");
+        debug_tokens(shell.token.head);
+        #endif
+        // Parsing
+        shell.cmd = parse_tokens(shell.token.head);
+        if (!shell.cmd)
+        {
+            cleanup_shell(&shell);
+            free(line);
+            continue;
+        }
+        // Debug opzionale
+        #ifdef DEBUG
+        printf("\nGenerated commands:\n");
+        debug_cmds(shell.cmd);
+        #endif
+        execute_command_list(&shell);
+        cleanup_shell(&shell);
+        free(line);
+    }
+    cleanup_shell(&shell);
     return (0);
 }
+
+// int main(int argc, char **argv, char **envp)
+// {
+// 	char *read_line;
+// 	t_env	*env = (t_env *){0};
+// 	t_shell	*root;
+// 	int i = 0;
+// 	//int a;
+// 	//char	*tryunset;
+
+// 	root = ft_calloc(1, sizeof(t_shell));
+// 	env = copy_env(envp);
+// 	root->env = env;
+// 	(void)argc;
+// 	(void)argv;
+// 	signal(SIGINT, sigint_handler);
+// 	signal(SIGQUIT, SIG_IGN);
+// 	while(1)
+// 	{
+// 		read_line = readline("");
+
+// 		if (read_line == NULL)
+// 		{
+// 			printf("\nexit\n");
+// 			free_env_list(env);
+// 			free (root);
+// 			break;
+// 		}
+// 		parser_builtin(root, read_line);
+// 		free(read_line);
+// 		i++;
+// 	}
+//     return (0);
+// }
